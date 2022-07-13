923d09725c686030f011823649489d07676697f4
cheriwhalen <cheri.whalen@leanderisd.org>
2022-07-13 13:40:50 -0500

    Initial Commit

---
+++ WeatherAppSummitGit/WeatherAppSummitGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSummitGitApp.swift
+//  WeatherAppSummitGit
+//
+//  Created by Cheri Whalen on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSummitGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
